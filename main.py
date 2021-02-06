18c87c2ea5864a510a91b552524729ca56fbd600
Robinborg <robin.borgatrom@gmail.com>
2021-02-06 15:44:00 +0100

    Setup

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_aboslute_error
+
+
